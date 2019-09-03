9b9e6c1dae204312987a29ffab05f99d20ef48d2
xy <625270910@qq.com>
2019-09-03 17:04:15 +0800

    1222222222222

--- back-end/sql/sakura.sql
+++ back-end/sql/sakura.sql
@@ -18,3 +18,22 @@ SHOW TABLES;
 DESC memeda_user;
 select * from memeda_user;
 
+/**轮播图**/
+CREATE TABLE memeda_carouse(
+    uid INT PRIMARY KEY AUTO_INCREMENT,
+    pic VARCHAR(32),
+    title VARCHAR(32),
+    text VARCHAR(32),
+    list  VARCHAR(32) 
+);
+INSERT INTO memeda_carouse VALUES(NULL,"/img/img01.png","/img/text01.svg","重塑生产力","经纬 M200 V2 系列");
+INSERT INTO memeda_carouse VALUES(NULL,"/img/img02.png","/img/text02.svg","随手掌控，一拍即合","Osmo Mobile 灵眸手机云台 3");
+INSERT INTO memeda_carouse VALUES(NULL,"/img/img03.png","/img/text03.svg","竞速视界，更快更清晰","DJI FPV 数字图传系统");
+INSERT INTO memeda_carouse VALUES(NULL,"/img/img04.png","/img/text04.png","轻装上阵，何惧挑战","Ronin SC 如影 SC 单手持微单稳定器");
+INSERT INTO memeda_carouse VALUES(NULL,"/img/img05.png","/img/text05.svg","玩出名堂","机甲大师 RoboMaster S1");
+INSERT INTO memeda_carouse VALUES(NULL,"/img/img06.png","/img/text06.svg","双屏一体，稳定如一","OSMO ACTION 灵眸运动相机");
+INSERT INTO memeda_carouse VALUES(NULL,"/img/img07.png","/img/text08.png","高瞻远瞩，以小见大","御 Mavic 2");
+INSERT INTO memeda_carouse VALUES(NULL,"/img/img08.png","/img/air.svg","随行新视界","御 Mavic Air");
+INSERT INTO memeda_carouse VALUES(NULL,"/img/img12.png","/img/t16.svg","大，有可为","T16 植保无人飞机");
+INSERT INTO memeda_carouse VALUES(NULL,"/img/img13.png","/img/ling.svg","转动随心，灵感不停","灵眸 Osmo 口袋云台相机");
+
